4f65a2dc83effd9336f6ce4c3d027e4f2e293db7
Andre Fonseca <afbfonseca@gmail.com>
2017-05-04 12:26:25 +0100

    Fixed algorithm

--- P02/src/divisor.ino
+++ P02/src/divisor.ino
@@ -7,20 +7,25 @@ byte dividend;
 // Output variables
 byte quotient;
 byte remainder;
+byte ready;
+byte div_zero;
 
 // Flip flop JK
 volatile boolean Q;
 boolean enabler;
 boolean selector;
 
-// Modules
-volatile byte I_Q;
-volatile byte R_Q;
-
 // Clock
-boolean mclock;
 unsigned long time_positive;
 unsigned long time_negative;
+
+byte I_D;
+byte I_Q;
+byte I_R;
+byte R_D;
+byte R_Q;
+byte R_R;
+boolean I;
 
 void setup() {
     Serial.begin(9600);
@@ -29,135 +34,113 @@ void setup() {
     // enables interrupts
     interrupts();
     initialize();
+    print_results("");
 }
 
 void initialize() {
     randomSeed(analogRead(A0));
-    divisor = random(16);
-    dividend = random(16);
+    // dividend = random(16);
+    dividend = 10;
+    // divisor = random(16);
+    divisor = 3;
+
     quotient = 0;
-    remainder = dividend;
+    remainder = 10;
 }
 
 void loop() {
-    functional_module(dividend, divisor, selector, enabler, mclock);
-
-    // TODO: Add functionality for when the Serial is available
-    // Print stuff and help menu
-
-    // if (Serial.available()) {
-    //
-    // }
+    control_module();
+    functional_module();
 }
 
-byte register_memory(byte D, boolean E, boolean CLK) {
-    byte _Q = D;
-    return _Q;
+byte MUX_2x1(boolean S, byte A, byte B) {
+    return S ? A : B;
 }
 
-void functional_module(byte D, byte d, boolean S, boolean E, boolean CLK) {
-    boolean I_selector = S;
-    boolean R_selector = S;
-    boolean I_enable = E;
-    boolean R_enable = E;
-    boolean I_clock = CLK;
-    boolean R_clock = CLK;
-
-    byte I_R = subtract(remainder, d);
-    byte I_Y = MUX_2x1(I_selector, D, I_R);
-    byte I_D = I_Y;
-
-    if (I_enable && !I_clock) {
-        I_Q = register_memory(I_D, I_enable, I_clock);
-    }
-
-    byte R_R = add(1, quotient);
-    byte R_Y = MUX_2x1(R_selector, 0, R_R);
-    byte R_D = R_Y;
-
-    if (R_enable && !R_clock) {
-        R_Q = register_memory(R_D, R_enable, R_clock);
-    }
+byte register_memory(boolean E, byte D, byte Q) {
+    return E ? D : Q;
 }
 
-boolean flip_flop_JK(boolean J, boolean K, boolean clk) {
+byte subtract(byte A, byte B) {
+    return A - B;
+}
+
+byte add(byte A, byte B) {
+    return A + B;
+}
+
+void functional_module() {
+    I = remainder >= divisor;
+
+    I_R = subtract(remainder, divisor);
+    I_D = MUX_2x1(selector, dividend, I_R);
+    I_Q = register_memory(enabler, I_D, I_Q);
+
+    R_R = add(1, quotient);
+    R_D = MUX_2x1(selector, 0, R_R);
+    R_Q = register_memory(enabler, R_D, R_Q);
+}
+
+boolean flip_flop_JK(boolean J, boolean K) {
     return Q & !K | !Q & J;
 }
 
-void control_module(boolean condition, boolean clk) {
-    if (!clk) {
-        return;
-    }
-
-    // left side of the condition
+void control_module() {
     selector = !Q;
-    // left or right side of the condition
-    enabler = !Q | condition;
+    enabler = !Q | I;
 
     boolean J = true;
-    boolean K = !condition;
-
-    Q = flip_flop_JK(J, K, clk);
+    boolean K = !I;
+    Q = flip_flop_JK(J, K);
 }
 
-// TODO: Review and debug this module
-// TODO: positive and negative MCLK functions can probably
-// be refactored to 1 function
-// Control module runs first (rising clock)
 void MCLK_positive() {
     unsigned long current_time = millis();
 
-    if (current_time - time_positive > DEBOUNCE_DELAY) {
-        mclock = true;
-        boolean condition = remainder >= divisor;
-        control_module(condition, mclock);
+    if (current_time - time_positive >= DEBOUNCE_DELAY) {
+
 
         if (!Q) {
             initialize();
+          	print_results("P >");
         }
 
         attachInterrupt(digitalPinToInterrupt(2), MCLK_negative, FALLING);
     }
 
+
     time_positive = current_time;
 }
 
-// TODO: Review and debug this module
-// Functional module runs last (falling clock)
 void MCLK_negative() {
     unsigned long current_time = millis();
 
-    if (current_time - time_negative > DEBOUNCE_DELAY) {
+    if (current_time - time_positive >= DEBOUNCE_DELAY) {
         if (enabler) {
             remainder = I_Q;
             quotient = R_Q;
+          	ready = !Q;
+          	print_results("N >");
         }
 
-        mclock = false;
-        boolean condition = remainder >= divisor;
-        control_module(condition, mclock);
+
         attachInterrupt(digitalPinToInterrupt(2), MCLK_positive, RISING);
     }
 
-    time_negative = current_time;
+    time_positive = current_time;
 }
 
-// Subtract module
-byte subtract(byte A, byte B) {
-    return A - B;
-}
-
-// Sum module
-byte add(byte A, byte B) {
-    return A + B;
-}
-
-// Multiplexer module
-byte MUX_2x1(boolean selector, byte input_0, byte input_1) {
-    switch(selector) {
-        case 0:
-            return input_1;
-        case 1:
-            return input_0;
-    }
+void print_results(String extra) {
+    Serial.println(extra);
+    Serial.print(" E ");
+    Serial.print(enabler);
+    Serial.print(" Q ");
+    Serial.print(quotient);
+    Serial.print(" R ");
+    Serial.print(remainder);
+  	Serial.print(" DZ ");
+  	Serial.print(div_zero);
+  	Serial.print(" RDY ");
+  	Serial.print(ready);
+    Serial.println();
 }
