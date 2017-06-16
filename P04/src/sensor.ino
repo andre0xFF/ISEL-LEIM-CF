55fd6c5e761f468b1dff4ae1342ff886aaab8374
Andre Fonseca <afbfonseca@gmail.com>
2017-06-16 22:35:33 +0100

    Sensor concept test

--- P04/src/sensor.ino
+++ P04/src/sensor.ino
@@ -1,23 +1,38 @@
 #include <Wire.h>
 
+// Addresses
 #define RS 0x01
 #define RW 0x02
 #define EN 0x04
 #define LIGHT 0x08
 #define LCD_ADDRESS 0x3F
 
+// Pins
 #define SENSOR_TRIGGER_PIN 4
 #define SENSOR_ECHO_PIN 2
+#define BUTTON_PIN 7
+
+// Autonomous systems states
+// Multiple autonomous systems may use WAITING state
+#define WAITING 0
+#define SENSOR_ECHO_UP 0
+#define SENSOR_ECHO_DOWN 1
+#define MEASURE_DISTANCE_TRIGGER 1
+#define BUTTON_PRESSED 1
+
+int sensor_echo_state = SENSOR_ECHO_DOWN;
+int measure_distance_state = MEASURE_DISTANCE_TRIGGER;
+int button_state = WAITING;
+
+// Variables
+#define SENSOR_TRIGGER_GAP 60
+#define BUTTON_DEBOUNCE_TIME 20
+
+boolean sensor_echo;
 long sensor_trigger;
-boolean sensor_echo = false;
-
-#define INTERRUPT_UP 0
-#define INTERRUPT_DOWN 1
-int interrupt_state = INTERRUPT_DOWN;
-
-// int button_state
-// #define WAITING 0
-// #define PRESSED 1
+long sensor_echo_time;
+long button_time;
+int measured_distance;
 
 void display_write_data_4(byte data) {
     Wire.beginTransmission(LCD_ADDRESS);
@@ -97,7 +112,7 @@ void display_clear() {
     display_write_command_8(0x01);
 }
 
-void display_set_cursor(byte line, bute column) {
+void display_set_cursor(byte line, byte column) {
     display_write_command_8(0x80 | line << 6 | column);
 }
 
@@ -107,7 +122,7 @@ void display_print_char(char data) {
 
 void display_print_string(String data) {
     for (int i = 0; i = data.length(); i++) {
-        display_print_char(data[i])
+        display_print_char(data[i]);
     }
 }
 
@@ -116,21 +131,107 @@ void display_print_int(int data) {
     display_print_string(s);
 }
 
-void sensor_generate_trigger() {
+void print_distance(double distance) {
+    // TODO
+    if (distance >= 2 && distance <= 400) {
+
+        return;
+    }
+
+    if (distance < 2) {
+
+        return;
+    }
+}
+
+boolean sensor_generate_trigger() {
+    if (millis() - sensor_trigger < SENSOR_TRIGGER_GAP) {
+        return false;
+    }
+
+    sensor_echo = false;
     sensor_trigger = millis();
     digitalWrite(SENSOR_TRIGGER_PIN, HIGH);
     delayMicroseconds(10);
     digitalWrite(SENSOR_TRIGGER_PIN, LOW);
+    return true;
 }
 
 void sensor_interrupt() {
-    if (interrupt_state == INTERRUPT_UP) {
-
+    if (sensor_echo_state == SENSOR_ECHO_UP) {
+        sensor_echo_time = micros();
+        attachInterrupt(digitalPinToInterrupt(SENSOR_ECHO_PIN), sensor_interrupt, FALLING);
+        sensor_echo_state = SENSOR_ECHO_DOWN;
         return;
     }
 
-    if (interrupt_state == INTERRUPT_DOWN) {
-
+    if (sensor_echo_state == SENSOR_ECHO_DOWN) {
+        sensor_echo = true;
+        sensor_echo_time = micros() - sensor_echo_time;
+        attachInterrupt(digitalPinToInterrupt(SENSOR_ECHO_PIN), sensor_interrupt, RISING);
+        sensor_echo_state = SENSOR_ECHO_UP;
         return;
     }
+}
+
+void measure_distance() {
+    if (measure_distance_state == WAITING) {
+        if (!sensor_echo) {
+            display_clear();
+            measure_distance_state = MEASURE_DISTANCE_TRIGGER;
+            return;
+        }
+
+        measured_distance = sensor_echo_time / 58;
+        measure_distance_state = MEASURE_DISTANCE_TRIGGER;
+        print_distance(measured_distance);
+        return;
+    }
+
+    if (measure_distance_state == MEASURE_DISTANCE_TRIGGER) {
+        boolean trigger = sensor_generate_trigger();
+
+        measure_distance_state = trigger ? WAITING : MEASURE_DISTANCE_TRIGGER;
+        return;
+    }
+}
+
+void button() {
+    boolean pressed = digitalRead(BUTTON_PIN);
+    long current_time = millis();
+
+    if (button_state == WAITING && pressed) {
+        if (current_time - button_time < BUTTON_DEBOUNCE_TIME) {
+            button_time = millis();
+            return;
+        }
+
+        button_state = BUTTON_PRESSED;
+    }
+
+    if (button_state == BUTTON_PRESSED && !pressed) {
+        if (current_time - button_time < BUTTON_DEBOUNCE_TIME) {
+            button_time = millis();
+            return;
+        }
+
+        button_state = WAITING;
+    }
+}
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(SENSOR_TRIGGER_PIN, OUTPUT);
+    pinMode(SENSOR_ECHO_PIN, INPUT);
+    pinMode(BUTTON_PIN, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(SENSOR_ECHO_PIN), sensor_interrupt, RISING);
+}
+
+void loop() {
+    if (Serial.available()) {
+        // TODO
+    }
+    measure_distance();
+    button();
 }
