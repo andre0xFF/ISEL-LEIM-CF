9b69c2ab5750ebb3f8689bed46fe02e79f74c8b9
Andre Fonseca <afbfonseca@gmail.com>
2017-06-18 22:19:40 +0100

    New state machine conditions check

--- P04/src/poc_button.ino
+++ P04/src/poc_button.ino
@@ -1,10 +1,10 @@
 #define STATE_WAITING 0
 #define STATE_BUTTON_CLICKED 1
-#define STATE_BUTTON_CHECKING 2
 #define PIN_BUTTON 7
 
 int button = STATE_WAITING;
 long button_timer;
+boolean button_status;
 
 void setup() {
     Serial.begin(9600);
@@ -19,29 +19,25 @@ void loop() {
     STATE MACHINES
 */
 void button_state_machine() {
-    boolean button_status = digitalRead(PIN_BUTTON);
-    Serial.println(button);
-    Serial.println(button_status);
-    
+    boolean button_status_previous = button_status;
+    button_status = digitalRead(PIN_BUTTON);
 
-    if (button == STATE_WAITING && button_status) {
-        button_timer = millis();
-        button = STATE_BUTTON_CHECKING;
-        on_button_checking();
-        return;
-    }
-
-    long timer = millis() - button_timer;
-
-    if (button == STATE_BUTTON_CHECKING && timer > 20 && button_status) {
-        button = STATE_BUTTON_CLICKED;
-        on_button_clicked();
-        return;
-    }
-
-    if (button == STATE_WAITING && !button_status || button == STATE_BUTTON_CLICKED && !button_status) {
-        button = STATE_WAITING;
+    if (button == STATE_WAITING) {
         on_button_waiting();
+        long timer = millis() - button_timer;
+
+        if (!button_status_previous && button_status && timer > 20) {
+            button = STATE_BUTTON_CLICKED;
+        }
+        return;
+    }
+
+    if (button = STATE_BUTTON_CLICKED) {
+        on_button_clicked();
+
+        if (true) {
+            button = STATE_WAITING;
+        }
         return;
     }
 }
